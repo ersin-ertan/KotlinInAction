8078e53b9a41178d447730a15aad65df51e3e5c4
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-08-09 15:05:19 -0400

    When, is, blocks, expressions, returns and smart casts. Finish enum when example with setOf usage.

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,9 @@
 import c01.Person
 import c02.Colour
 import c02.Colour.*
+import c02.creation
+import c02.javaEval
+import c02.whenWithoutArgs
 
 fun main(args: Array<String>){ // top level function
     println(
@@ -9,8 +12,11 @@ fun main(args: Array<String>){ // top level function
 //    c02.stringTemplates()
 //    practice.DC("Tom").multiGet()
 //    c02.enumFunction()
-    c02.getMapper(Colour.FLASHING)
+//    c02.getMapper(Colour.FLASHING)
+//    c02.mixedMapper(ORANGE, RED) // notice the order is mixed
+//    whenWithoutArgs(BLACK, FLASHING) // can mix order unless explicit
+    javaEval(creation())
     )
 
-    c02.getMapper(BLACK) // import Colour.*
+//    c02.getMapper(BLACK) // import Colour.*
 }
