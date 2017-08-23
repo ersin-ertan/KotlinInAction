aa7c6e30a9f2caf0742fa4c563ece99e3c4fd82c
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-08-23 00:49:54 -0400

    Functional apis for collections (mostly eager)

---
+++ src/c05/5_3_0_LazyCollectionOperanionsSequencecs.kt
@@ -0,0 +1,5 @@
+package c05
+
+// performance of chained collection operations
+// map and filter create eager intermediate collections.
+// Sequences give you an alternative way to perform computations, avoiding intermediate temp objects
